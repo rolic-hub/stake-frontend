b114a2d70a9c1b2901c18ef303b39bc0dfa3782f
rolic-hub <enyiomaeze09@gmail.com>
2022-08-11 21:36:38 +0100

    update

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+
+export default function Layout() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex mt-20">
+        <div className="w-96 h-screen border-r border-black">
+            
+        </div>
+        <div className="flex h-screen ">
+          <p>Home page</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,70 @@
+import React from "react";
+import { Select } from "@web3uikit/core";
+import { ConnectButton } from "@web3uikit/web3";
 
-
-export default function Navbar(){
-    return(
-        <div>
-            <nav>
-                
-            </nav>
-        </div>
-    )
+export default function Navbar() {
+  const options = [
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+    {
+      id: "emoji",
+      label: "Emoji",
+      prefix: "🏴󠁧󠁢󠁷󠁬󠁳󠁿",
+    },
+    {
+      id: "txt",
+      label: "TXT",
+      prefix: "TXT",
+    },
+    {
+      id: "dapp",
+      label: "dApp",
+      //prefix: <SvgServer fill="#68738D" />,
+    },
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+    {
+      id: "discord",
+      label: "Discord",
+      //prefix: <SvgDiscord fill="#68738D" />,
+    },
+  ];
+  return (
+    <div className="w-full fixed top-0 left-0 shadow-md border-b border-black h-20 py-3 absolute">
+      <div className=" flex items-center justify-between">
+        {" "}
+        <h1 className="text-3xl px-20">Staking App</h1>
+        <Select
+          style={{ marginLeft: "550px" }}
+          label="Label Text"
+          onBlurTraditional={function noRefCheck() {}}
+          onChange={function noRefCheck() {}}
+          onChangeTraditional={function noRefCheck() {}}
+          options={options}
+        />
+        <ConnectButton style={{marginRight:"100px"}} />
+      </div>
+    </div>
+  );
 }
